bb0e5e2a49e3229db1631ad797a266eb4aa9ff7d
RolandMarton <roland111187@gmail.com>
2025-12-05 17:15:48 +0100

    Restructure the folders

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ import notesRoutes from './routes/notesRoutes.js';
 
 const app = express();
 
-app.use("/api/notes", notesRoutes);
+app.use('/api/notes', notesRoutes);
 
 app.listen(5001, () => {
 	console.log('Server is running on port 5001');
