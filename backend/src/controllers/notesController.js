64e50eae8db9c1cd3eb60533dd17afa9553ba8f4
RolandMarton <roland111187@gmail.com>
2025-12-15 16:16:57 +0100

    Get note by id added

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -2,11 +2,23 @@ import Note from '../models/Note.js';
 
 export async function getAllNotes(req, res) {
 	try {
-		const notes = await Note.find();
+		const notes = await Note.find().sort({ createdAt: -1 }); // Showing the latest first
 
 		res.status(200).json(notes);
 	} catch (error) {
 		console.error('Error fetching notes:', error);
+		res.status(500).json({ message: 'Internal server error' });
+	}
+}
+
+export async function getNoteById(req, res) {
+	try {
+		const note = await Note.findById(req.params.id);
+		if (!note) return res.status(404).json({ message: 'Note not found' });
+
+		res.status(200).json(note);
+	} catch (error) {
+		console.error('Error in getNoteById controller', error);
 		res.status(500).json({ message: 'Internal server error' });
 	}
 }
