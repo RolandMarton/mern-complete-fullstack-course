3b16bab3538a2c1637eeae9e811e2dc0dc1591bf
RolandMarton <roland111187@gmail.com>
2025-12-18 16:29:57 +0100

    Add comments and restructure server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,19 +4,23 @@ import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
 import ratelimit from './middlewares/rateLimiter.js';
 
+// Load environment variables
 dotenv.config();
 
+// Initialize Express app
 const app = express();
 const PORT = process.env.PORT || 5001;
-
-connectDB();
 
 // Middleware
 app.use(express.json());
 app.use(ratelimit);
 
+// Routes
 app.use('/api/notes', notesRoutes);
 
-app.listen(PORT, () => {
-	console.log('Server is running on port', PORT);
+// Start server after DB connection
+connectDB().then(() => {
+	app.listen(PORT, () => {
+		console.log('Server is running on port', PORT);
+	});
 });
